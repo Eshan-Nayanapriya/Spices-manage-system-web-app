bc4db94692eccb0ca4dd4b7adbc947fc403b6150
Menusha Thathsara <161927072+Menusha2k1@users.noreply.github.com>
2024-04-25 00:37:43 +0530

    Create enquiryModel.js

---
+++ backend/models/enquiryModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const enquirySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    phone: { type: Number, required: true },
+    email: { type: String, required: true },
+    product: { type: String, required: true },
+    description: { type: String, required: true },
+
+})
+const enquiryModel = mongoose.models.enquiry || mongoose.model("enquiry",enquirySchema);
+
+export default enquiryModel;
