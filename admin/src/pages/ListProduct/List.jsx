61af450687e54165ab871b603bdb74644c9b4d59
Eshan-Nayanapriya <eshannayanapriyaprofessional@gmail.com>
2024-04-24 23:48:51 +0530

    list change

--- admin/src/pages/ListProduct/List.jsx
+++ admin/src/pages/ListProduct/List.jsx
@@ -10,7 +10,7 @@ const List = ({url}) => {
   const [list,setList] = useState([]);
 
   const fetechList = async () => {
-    const response = await axios.get(`${url}/api/food//list`);
+    const response = await axios.get(`${url}/api/food/list`);
     console.log(response.data);
     if(response.data.success){
       setList(response.data.data)
