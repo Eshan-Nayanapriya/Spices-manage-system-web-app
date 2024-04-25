21b8c0c6ab0be2f9d93e1aeb8ace76317d3e656c
Eshan-Nayanapriya <eshannayanapriyaprofessional@gmail.com>
2024-04-25 10:19:06 +0530

    updated admin dashboard

--- frontend/src/assets/frontend_assets/assets.js
+++ frontend/src/assets/frontend_assets/assets.js
@@ -4,7 +4,6 @@ import heade_img from './heade_img.png'
 import search_icon from './search_icon.png'
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
-import menu_3 from './menu_3.png'
 import menu_4 from './menu_4.png'
 import menu_5 from './menu_5.png'
 import menu_6 from './menu_6.png'
@@ -85,27 +84,27 @@ export const assets = {
 
 export const menu_list = [
     {
-        menu_name: "Chilli Powder",
+        menu_name: "Powder",
         menu_image: menu_1
     },
     {
-        menu_name: "Chilli Pieces",
+        menu_name: "Pieces",
         menu_image: menu_2
     },
     {
-        menu_name: "Pepper Powder",
-        menu_image: menu_3
+        menu_name: "Flour",
+        menu_image: menu_6
     },
     {
-        menu_name: "Turmeric Powder",
+        menu_name: "Oil",
         menu_image: menu_4
     },
     {
-        menu_name: "Sunflower Seeds",
+        menu_name: "Seeds",
         menu_image: menu_5
     },
     {
-        menu_name: "Flour",
+        menu_name: "Other",
         menu_image: menu_6
     }]
 
