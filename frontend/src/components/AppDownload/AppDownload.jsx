5efc0827cd7cbe9738d777f4e12cef86a7eb140c
Eshan-Nayanapriya <eshannayanapriyaprofessional@gmail.com>
2024-04-25 08:43:53 +0530

    app name changed

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/frontend_assets/assets'
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-      <p>For Better Experience Download <br/> Tomato App</p>
+      <p>For Better Experience Download <br/> Sahan Grinding Mills App</p>
       <div className="app-download-platforms">
         <img src={assets.play_store} alt="" />
         <img src={assets.app_store} alt="" />
