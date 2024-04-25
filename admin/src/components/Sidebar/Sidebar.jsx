a6433e5fee0cc32eaf6f7a44e4c75f228b2bff12
Menusha Thathsara <161927072+Menusha2k1@users.noreply.github.com>
2024-04-25 09:51:31 +0530

    bug fixes

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -4,43 +4,43 @@ import { assets } from '../../assets/assets'
 import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-        <div className="sidebar-options">
-            <NavLink to="/add" className="sidebar-option">
-                <img src={assets.add_icon} alt="" />
-                <p>Add Items</p>
-            </NavLink>
-            <NavLink to="/list" className="sidebar-option">
-                <img src={assets.order_icon} alt="" />
-                <p>List Items</p>
-            </NavLink>
-            <NavLink to="/orders" className="sidebar-option">
-                <img src={assets.add_icon} alt="" />
-                <p>Orders</p>
-            </NavLink>
-            <NavLink to="/supplier" className="sidebar-option">
-                <img src={assets.add_icon} alt="" />
-                <p>Supplier</p>
-            </NavLink>
-            <NavLink to="/paymentRequests" className="sidebar-option">
-                <img src={assets.add_icon} alt="" />
-                <p>Payment</p>
-            </NavLink>
+    return (
+        <div className='sidebar'>
+            <div className="sidebar-options">
+                <NavLink to="/add" className="sidebar-option">
+                    <img src={assets.add_icon} alt="" />
+                    <p>Add Items</p>
+                </NavLink>
+                <NavLink to="/list" className="sidebar-option">
+                    <img src={assets.order_icon} alt="" />
+                    <p>List Items</p>
+                </NavLink>
+                <NavLink to="/orders" className="sidebar-option">
+                    <img src={assets.add_icon} alt="" />
+                    <p>Orders</p>
+                </NavLink>
+                <NavLink to="/supplier" className="sidebar-option">
+                    <img src={assets.add_icon} alt="" />
+                    <p>Supplier</p>
+                </NavLink>
+                <NavLink to="/paymentRequests" className="sidebar-option">
+                    <img src={assets.add_icon} alt="" />
+                    <p>Payment</p>
+                </NavLink>
 
-            <NavLink to="/displayenquiry" className="sidebar-option">
-                <img src={assets.add_icon} alt="" />
-                <p>Enquries</p>
+                <NavLink to="/displayenquiry" className="sidebar-option">
+                    <img src={assets.add_icon} alt="" />
+                    <p>Enquries</p>
+                </NavLink>
+                <NavLink to="/promotionManagement" className="sidebar-option">
+                    <img src={assets.add_icon} alt="" />
+                    <p>Promotion Management</p>
 
-            <NavLink to="/promotionManagement" className="sidebar-option">
-                <img src={assets.add_icon} alt="" />
-                <p>Promotion Management</p>
+                </NavLink>
+            </div>
 
-            </NavLink>
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default Sidebar
