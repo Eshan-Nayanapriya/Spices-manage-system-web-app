069735a0f3263f19092ce7b18528cabfb79cc3fc
Eshan-Nayanapriya <eshannayanapriyaprofessional@gmail.com>
2024-05-04 00:38:43 +0530

    css

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -11,8 +11,7 @@ const Header = () => {
     <div className='header'>
       <div className="header-contents">
         <h2>Order your favourite spices here</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic facilis, ipsum suscipit asperiores consequatur iusto soluta, at ducimus nam aspernatur architecto deserunt ipsa dolore sint. Eveniet vitae deleniti itaque, quaerat praesentium doloribus molestiae id dolores.
-        </p>
+        <p>Join us on a flavorful journey and experience the difference that Sahan Grinding Mills spices can make in your kitchen. Order your favorite spices today and let the essence of excellence infuse every dish. Spice up your culinary adventures with Sahan Grinding Mills!</p>
         <a href="#explore-menu" onClick={()=> setMenu("menu")} className={menu==="menu"?"active":""}><button>View Menu</button></a>
       </div>
     </div>
