e041c6fffda45d469753a94c7f05725b8a905a61
Udan Pasindu Jayasekara <151393273+Udanpasindu@users.noreply.github.com>
2024-05-13 13:09:50 +0530

    css supplier pro

--- admin/src/pages/Supplier/SupplierProfile.jsx
+++ admin/src/pages/Supplier/SupplierProfile.jsx
@@ -2,96 +2,90 @@ import { useNavigate } from "react-router-dom";
 
 const SupplierProfile = () => {
   let navigate = useNavigate();
-  var SName = "Udan Pasindu"
-  var SMobile = "0717325275"
-  var SEmail = "Udanpasindu934@gmail.com"
-  var SReg = "S003"
+  var SName = "Udan Pasindu";
+  var SMobile = "0717325275";
+  var SEmail = "Udanpasindu934@gmail.com";
+  var SReg = "S003";
 
   return (
     <div 
-  style={{
-    
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    alignItems: "center",
-    height: "100vh",
-    marginLeft:"300px",
-    marginTop:"60px",
-    backgroundColor: "#fff",
-  }}
->
-  <div 
-    style={{
-      backgroundColor: "rgba(5, 8, 2, 0.75)",
-      borderRadius: "10px",
-      padding: "100px",
-    }}
-  >
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1 style={{ color: "#fff" }}>Supplier Profile</h1>
-    </div>
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div>
-        <div style={{ color: "#fff", textAlign: "center", padding: "20px" }}>
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width:"2000px",
+        height: "100vh",
+        background: "linear-gradient(139deg, #000, #c3cfe2)",
+      }}
+    >
+      <div 
+        style={{
+          backgroundColor: "#fff",
+          borderRadius: "10px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          width: "800px",
           
-          
+        }}
+      >
+        <div 
+          style={{
+            backgroundColor: "#394a6d",
+            borderTopLeftRadius: "10px",
+            borderTopRightRadius: "10px",
+            padding: "50px",
+          }}
+        >
+          <h1 style={{ color: "#fff", margin: "10" }}>Supplier Profile</h1>
         </div>
-      </div>
-      <div style={{ backgroundColor: "rgba(0, 0, 0, 0.25)", borderRadius: "10px", padding: "20px" }}>
-        <div style={{ textAlign: "center", padding: "100px" }}>
-          <button 
-            onClick={() => navigate("/Supplier/RawMreq")} 
-            style={{
-              padding: "30px 60px",
-              backgroundColor: "tomato",
-              color: "#fff",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-              opacity: "1",
-            }}
-          >
-            Supply Requests
-          </button>
-          <br />
-          <br />
-          <button 
-            onClick={() => navigate("/Supplierpro/RawMreqres")} 
-            style={{
-              padding: "30px 60px",
-              backgroundColor: "tomato",
-              color: "#fff",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-              opacity: "1",
-            }}
-          >
-            Manage Supply Requests
-          </button>
-          <br />
-          <br />
-          <button 
-            onClick={() => navigate("/AddPaymentRequest")} 
-            style={{
-              padding: "30px 60px",
-              backgroundColor: "tomato",
-              color: "#fff",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-              opacity: "1",
-            }}
-          >
-            Requests Payments
-          </button>
+        <div style={{ padding: "100px" }}>
+          
+          <div style={{  flexDirection: "column" }}>
+            <button 
+              onClick={() => navigate("/Supplier/RawMreq")} 
+              style={{
+                padding: "30px 200px",
+                margin: "5px",
+                backgroundColor: "tomato",
+                color: "#fff",
+                border: "none",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Supply Requests
+            </button>
+            <button 
+              onClick={() => navigate("/Supplierpro/RawMreqres")} 
+              style={{
+                padding: "30px 175px",
+                margin: "5px",
+                backgroundColor: "tomato",
+                color: "#fff",
+                border: "none",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Manage Supply Requests
+            </button>
+            <button 
+              onClick={() => navigate("/AddPaymentRequest")} 
+              style={{
+                padding: "30px 192px",
+                margin: "5px",
+                backgroundColor: "tomato",
+                color: "#fff",
+                border: "none",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Requests Payments
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
-  
   );
 };
 
