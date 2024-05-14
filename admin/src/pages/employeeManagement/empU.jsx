3c0be34684f02697299a230acbbc6b784a670e5d
PrageethTorin <148193416+PrageethTorin@users.noreply.github.com>
2024-05-14 11:20:22 +0530

    update backend

--- admin/src/pages/employeeManagement/empU.jsx
+++ admin/src/pages/employeeManagement/empU.jsx
@@ -1,61 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from "react-router-dom"; 
 import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 
 function EmpU() {
-  const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
-  //const lUser = loggedInUser
   const { id } = useParams();
-  //console.log("lsss ", lUser)
+  
   useEffect(() => {
-    
-      axios.get(`http://localhost:4000/User/getEmpuser/${id}`)
-        .then(response => {
-          setUser(response.data);
-        })
-        .catch(error => {
-          console.error('Error fetching user:', error);
-        });
-    
+    axios.get(`http://localhost:4000/User/getEmpuser/${id}`)
+      .then(response => {
+        setUser(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching user:', error);
+      });
   }, []);
 
   const handleDelete = (id) => {
-    const confirmDelete = window.confirm(" do you want to delete?");
+    const confirmDelete = window.confirm("Do you want to delete?");
     if (confirmDelete) {
-        axios.delete(`http://localhost:4000/User/deleteUser/${id}`)
-            .then(res => {
-                console.log(res);
-                setUsers(users.filter(user => user._id !== id));
-                setFilteredUsers(filteredUsers.filter(user => user._id !== id)); // Update filteredUsers as well
-                navigate('/employeeManagement');
-            })
-            .catch(err => console.log(err))
+      axios.delete(`http://localhost:4000/User/deleteUser/${id}`)
+        .then(res => {
+          console.log(res);
+          navigate('/emplogin');
+        })
+        .catch(err => console.log(err))
     }
   }
   
   return (
-    <div className="container mt-5">
-      <h2>User Details</h2>
-      <div>
-        <p><strong>Name:</strong> {user?.name}</p>
-        <p><strong>Email:</strong> {user?.email}</p>
-        <p><strong>Age:</strong> {user?.age}</p>
-        <p><strong>Job Role:</strong> {user?.jobroll}</p>
-        <p><strong>Bank:</strong> {user?.bank}</p>
-        <p><strong>Account Number:</strong> {user?.accountNumber}</p>
-        <div className="btn-group" role="group">
-                    <Link to={`/empupdate/${id}`} className='btn btn-success'>Update</Link>
-                    <button onClick={() => handleDelete(id)} className='btn btn-danger'>Delete</button>
-                  </div>
-                  <Link to="/salaryy" className='btn btn-warning'>Home</Link>          
-
+    <div style={{ marginTop: '50px', fontFamily: 'Arial, sans-serif', display: 'flex', justifyContent: 'center' , width: '100%',height:'100%'}}>
+      <div className="card" style={{ margin: '0 auto' }}>
+        <div className="card-body">
+          <h2 className="card-title" style={{ color: '#333', fontWeight: 'bold', marginBottom: '20px' }}>User Details</h2>
+          <p className="card-text"><strong>Name:</strong> {user?.name}</p>
+          <p className="card-text"><strong>Email:</strong> {user?.email}</p>
+          <p className="card-text"><strong>Age:</strong> {user?.age}</p>
+          <p className="card-text"><strong>Job Role:</strong> {user?.jobroll}</p>
+          <p className="card-text"><strong>Bank:</strong> {user?.bank}</p>
+          <p className="card-text"><strong>Account Number:</strong> {user?.accountNumber}</p>
+          <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+            <button onClick={() => handleDelete(id)} style={{ padding: '10px 20px', backgroundColor: '#dc3545', border: 'none', borderRadius: '4px', cursor: 'pointer', color: '#fff' }}>Delete</button>
+          </div>
+          <p className="card-text" style={{ color: '#333', fontWeight: 'bold' }}>Email us with your emp ID and name if you want to update: sahanadmin@gmail.com</p>
+        </div>
       </div>
-      
     </div>
   );
 }
