76d6e9f020cab79c5727fc81df8f809a8529dc3d
Udan Pasindu Jayasekara <151393273+Udanpasindu@users.noreply.github.com>
2024-05-14 13:17:37 +0530

    css supplier profile

--- admin/src/pages/Supplier/SupplierProfile.jsx
+++ admin/src/pages/Supplier/SupplierProfile.jsx
@@ -16,19 +16,10 @@ const SupplierProfile = () => {
   return (
     <div style={{ textAlign: "center" }}>
       <div style={{ backgroundColor: "rgba(0, 0, 0, 0.1)", padding: "20px", borderRadius: "10px", maxWidth: "600px", marginLeft: "450px", marginTop: "50px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" }}>
-        <h1 style={{ color: "#333", marginBottom: "30px", fontSize: "2rem",marginLeft: "-100px", }}>Supplier Profile</h1>
-        <div
-          style={{
-            backgroundColor: "grey",
-            borderRadius: "20px",
-            marginLeft: "-50px",
-            padding: "40px",
-            maxWidth: "800px",
-            width: "100%",
-            boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
-          }}
-        >
-        <form style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <form style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <h1 style={{ color: "#333", marginBottom: "30px", fontSize: "2rem"}}>Supplier Profile</h1>
+        
+       
           <button type="button" onClick={() => navigate("/Supplier/RawMreq")} style={{ padding: "40px 80px",
            backgroundColor: "#007bff", color: "#fff", border: "none", borderRadius: "5px", marginBottom: "10px", width: "100%" }}>Supply Requests</button>
           <button type="button" onClick={() => navigate("/Supplierpro/RawMreqres")} style={{ padding: "40px 80px",
@@ -37,10 +28,10 @@ const SupplierProfile = () => {
            backgroundColor: "#dc3545", color: "#fff", border: "none", borderRadius: "5px", marginBottom: "10px", width: "100%" }}>Requests Payments</button>
         </form>
         </div>
-        <p style={{ fontSize: "1.2rem",marginTop:"60px" ,marginLeft:"-100px"}}>Current Date and Time: {dateTime.toLocaleString()}</p>
+        <p style={{ fontSize: "1.2rem",marginTop:"60px" ,marginLeft:"450px"}}>Current Date and Time: {dateTime.toLocaleString()}</p>
         
       </div>
-    </div>
+    
   );
 };
 
