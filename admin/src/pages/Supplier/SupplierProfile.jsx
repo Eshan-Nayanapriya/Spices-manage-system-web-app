6580fee595f0998ea5e353e27ff28ba8f9bb1301
Udan Pasindu Jayasekara <151393273+Udanpasindu@users.noreply.github.com>
2024-05-14 11:56:14 +0530

    css supplier pro

--- admin/src/pages/Supplier/SupplierProfile.jsx
+++ admin/src/pages/Supplier/SupplierProfile.jsx
@@ -1,7 +1,9 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SupplierProfile = () => {
   let navigate = useNavigate();
+<<<<<<< Updated upstream
  
 
   return (
@@ -82,7 +84,45 @@ const SupplierProfile = () => {
               Requests Payments
             </button>
           </div>
+=======
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <div style={{ backgroundColor: "rgba(0, 0, 0, 0.1)", padding: "20px", borderRadius: "10px", maxWidth: "600px", marginLeft: "445px", marginTop: "10px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" }}>
+        <h1 style={{ color: "#333", marginBottom: "30px", fontSize: "2rem" }}>Supplier Profile</h1>
+        <div
+          style={{
+            backgroundColor: "grey",
+            borderRadius: "20px",
+            marginLeft:"-10px",
+            padding: "80px",
+            maxWidth: "800px",
+            width: "100%",
+            boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+        <form style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <button type="button" onClick={() => navigate("/Supplier/RawMreq")} style={{ padding: "40px 80px", 
+          backgroundColor: "#007bff", color: "#fff", border: "none", borderRadius: "5px", marginBottom: "10px", width: "100%" }}>Supply Requests</button>
+          <button type="button" onClick={() => navigate("/Supplierpro/RawMreqres")} style={{ padding: "40px 80px",
+           backgroundColor: "#28a745", color: "#fff", border: "none", borderRadius: "5px", marginBottom: "10px", width: "100%" }}>Manage Supply Requests</button>
+          <button type="button" onClick={() => navigate("/AddPaymentRequest")} style={{ padding: "40px 80px",
+           backgroundColor: "#dc3545", color: "#fff", border: "none", borderRadius: "5px", marginBottom: "10px", width: "100%" }}>Requests Payments</button>
+        </form>
+>>>>>>> Stashed changes
         </div>
+        <br></br>
+        <br></br>
+        <p style={{ fontSize: "1.2rem" }}>Current Date and Time: {dateTime.toLocaleString()}</p>
       </div>
     </div>
   );
