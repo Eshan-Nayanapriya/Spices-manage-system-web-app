3e38e787e85db3c69d79c37a9d888efa85209322
Udan Pasindu Jayasekara <151393273+Udanpasindu@users.noreply.github.com>
2024-05-14 11:57:29 +0530

    css supplier pro new

--- admin/src/pages/Supplier/SupplierProfile.jsx
+++ admin/src/pages/Supplier/SupplierProfile.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SupplierProfile = () => {
   let navigate = useNavigate();
-<<<<<<< Updated upstream
- 
+  var SName = "Udan Pasindu";
+  var SMobile = "0717325275";
+  var SEmail = "Udanpasindu934@gmail.com";
+  var SReg = "S003";
 
   return (
     <div
@@ -84,45 +85,7 @@ const SupplierProfile = () => {
               Requests Payments
             </button>
           </div>
-=======
-  const [dateTime, setDateTime] = useState(new Date());
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDateTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div style={{ textAlign: "center" }}>
-      <div style={{ backgroundColor: "rgba(0, 0, 0, 0.1)", padding: "20px", borderRadius: "10px", maxWidth: "600px", marginLeft: "445px", marginTop: "10px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" }}>
-        <h1 style={{ color: "#333", marginBottom: "30px", fontSize: "2rem" }}>Supplier Profile</h1>
-        <div
-          style={{
-            backgroundColor: "grey",
-            borderRadius: "20px",
-            marginLeft:"-10px",
-            padding: "80px",
-            maxWidth: "800px",
-            width: "100%",
-            boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
-          }}
-        >
-        <form style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <button type="button" onClick={() => navigate("/Supplier/RawMreq")} style={{ padding: "40px 80px", 
-          backgroundColor: "#007bff", color: "#fff", border: "none", borderRadius: "5px", marginBottom: "10px", width: "100%" }}>Supply Requests</button>
-          <button type="button" onClick={() => navigate("/Supplierpro/RawMreqres")} style={{ padding: "40px 80px",
-           backgroundColor: "#28a745", color: "#fff", border: "none", borderRadius: "5px", marginBottom: "10px", width: "100%" }}>Manage Supply Requests</button>
-          <button type="button" onClick={() => navigate("/AddPaymentRequest")} style={{ padding: "40px 80px",
-           backgroundColor: "#dc3545", color: "#fff", border: "none", borderRadius: "5px", marginBottom: "10px", width: "100%" }}>Requests Payments</button>
-        </form>
->>>>>>> Stashed changes
         </div>
-        <br></br>
-        <br></br>
-        <p style={{ fontSize: "1.2rem" }}>Current Date and Time: {dateTime.toLocaleString()}</p>
       </div>
     </div>
   );
