70ba6810b6d56c1b50a41a36a567c5803db9cea0
Apsara2002 <125apsararanahansi@gmail.com>
2024-05-15 18:02:58 +0530

    search remove

--- frontend/src/components/FoodDisplay/foodDisplay.jsx
+++ frontend/src/components/FoodDisplay/foodDisplay.jsx
@@ -1,48 +1,26 @@
-import React, { useState } from 'react';
-import './foodDisplay.css';
-import { useContext } from 'react';
-import { StoreContext } from '../../context/StoreContext';
-import FoodItem from '../FoodItem/FoodItem';
+import React, { useState } from 'react'
+import './foodDisplay.css'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 import search from '../../assets/frontend_assets/search.png';
 
-const FoodDisplay = ({ category }) => {
-    const { food_list } = useContext(StoreContext);
-    const [searchQuery, setSearchQuery] = useState('');
+const FoodDisplay = ({category}) => {
 
-    const filteredFoodList = food_list.filter(item =>
-        (category === "All" || category === item.category) &&
-        item.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-
-    return (
-        <div className='food-display' id='food-display'>
-            <div className="header-container">
-                <h2>Top Products Near You...</h2>
-                <div className="search-container">
-                    <img src={search} alt="search" />
-                    <input
-                        type="text"
-                        placeholder="Search products..."
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                </div>
-                <br />
-            </div>
-            <div className="food-display-list">
-                {filteredFoodList.map((item, index) => (
-                    <FoodItem
-                        key={index}
-                        id={item._id}
-                        name={item.name}
-                        price={item.price}
-                        image={item.image}
-                        quantity={item.quantity}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+    const {food_list} = useContext(StoreContext)
+ 
+  return (
+    <div className='food-display' id='food-display'>
+      <h2>Top Products Near You...</h2>
+      <div className="food-display-list">
+        {food_list.map((item,index)=>{
+            if(category==="All" || category===item.category){
+                return <FoodItem key={index} id={item._id} name={item.name} price={item.price} image={item.image} quantity={item.quantity}/>
+            }  
+        })}
+      </div>
+    </div>
+  )
 }
 
-export default FoodDisplay;
+export default FoodDisplay
