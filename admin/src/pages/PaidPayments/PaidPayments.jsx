bc1ce6221dc9fbb8136d7fd350279bd0fa370dfc
Eshan Nayanapriya <161910749+Eshan-Nayanapriya@users.noreply.github.com>
2024-05-15 20:04:44 +0530

    batta changes delivered filter

--- admin/src/pages/PaidPayments/PaidPayments.jsx
+++ admin/src/pages/PaidPayments/PaidPayments.jsx
@@ -174,7 +174,7 @@ const PaidPayments = () => {
             <button style={{ marginLeft: "20px" }} className='pprpt-btn' onClick={downloadReport}>Download Report</button><br/>
             {/* <Link to={"/Pdfupload"}><button style={{ marginLeft: "20px" }} className='pprpt-btn'>Upload Report</button></Link> */}
             <Link to={"/OrderPayments"}>
-                <button className='orderpsymentsbtn'>Order Payments</button>
+                <button className='orderpaymentsbtn'>Completed Order Payments</button>
             </Link>
         </div>
     )
