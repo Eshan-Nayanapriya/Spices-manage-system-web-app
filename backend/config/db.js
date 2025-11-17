9e86d2e6a607fd636ead587846d5a6ec0b10e252
Eshan Nayanapriya <eshannayanapriyaprofessional@gmail.com>
2025-11-17 11:51:55 +0530

    Refactor database connection to use environment variable for MongoDB URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://admin:90PE5bfjEryoIUOX@cluster0.zvszq2r.mongodb.net/").then(() =>
-    console.log("Connected to MongoDB"));
-}
+  await mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"));
+};
